0e6f80e4788ec98433e1b827823a07a7a4354d8d
sahil <sahilkukreja1@gmail.com>
2019-07-06 19:45:31 +0530

    api intigrated-working

--- src/app/components/home-form/home-form.component.ts
+++ src/app/components/home-form/home-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { HomeServiceService } from '/Users/sahilkukreja/visatofly/29052019/ts-web/src/app/home-service.service';
 
 
 @Component({
@@ -11,11 +12,14 @@ styleUrls: ['./home-form.component.css']
 export class HomeFormComponent  {
 
 
-  constructor(private router: Router,private httpClient: HttpClient) { }
-  
+  constructor(private router: Router,private httpClient: HttpClient,private myservice: HomeServiceService ) { }
+    public myobj:any;
     public show:boolean = false;
   
-    ngOnInit () { }
+    ngOnInit () { 
+      this.myobj= this.myservice.getHomelanding();
+      console.log(this.myobj);
+    }
   
     toggle() {
       this.show = !this.show;
--- src/app/home-service.service.ts
+++ src/app/home-service.service.ts
@@ -21,7 +21,7 @@ export class HomeServiceService {
     return this.http.get('https://jsonplaceholder.typicode.com/users')
   }
   getHomelanding() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users',{headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'), observe: "response"})
+    return this.http.get('http://staging.visa2fly.com:8080/visa2fly-Backend-0.0.1-SNAPSHOT/info/landing')
     .subscribe( (data) => console.log(JSON.stringify(data)));
   }
   getrequirements() {
