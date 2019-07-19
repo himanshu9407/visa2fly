6132b680e38def2800810aeec1e814c446085b16
sahil <sahilkukreja1@gmail.com>
2019-07-20 03:32:00 +0530

    interface for api

--- src/app/home-service.service.ts
+++ src/app/home-service.service.ts
@@ -1,38 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
-import { map, catchError } from 'rxjs/operators';
-
-
 import { HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs";  
+import { requirementData } from './interfaces/requirement';
+import { home_formData } from './interfaces/home_formData';
 
 headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
 @Injectable({
   providedIn: 'root'
 })
 export class HomeServiceService {
-  //  httpOptions = {
-  //   headers: new HttpHeaders({
-  //     'Content-Type':  'application/json',
-  //   })
-  // };
-  url:String='http://staging.visa2fly.com:8080/visa2fly-Backend-0.0.1-SNAPSHOT/info/';
-  constructor(private http: HttpClient) { }
-  getUsers() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users')
-  }
-  getHomelanding() {
-    this.http.get(this.url+'landing')
-    .pipe(map((response : Response) => {
-        return response.json();   
-    }));
-  }
-  getrequirements() {
-    this.http.get('http://staging.visa2fly.com:8080/visa2fly-Backend-0.0.1-SNAPSHOT/info/Australia')
-    .pipe(map((response : Response) => {
-        return response.json();   
-    }));
-  }
 
- 
+  baseUrl:String='http://staging.visa2fly.com:8080/visa2fly-Backend-0.0.1-SNAPSHOT/info/';
+  constructor(private http: HttpClient) { }
+  get_requriements() : Observable<requirementData>{
+    return this.http.get<requirementData>(this.baseUrl+'Austrailia');
+  }
+  get_landing() : Observable<home_formData>{
+    return this.http.get<home_formData>(this.baseUrl+'landing');
+  }
 }
