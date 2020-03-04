ceaa85d02c3e514b86d745ba791501b62595a505
CodeDriller <me@codedriller.com>
2020-03-04 12:59:38 +0530

    skip for a moment

--- src/app/components/world/world.component.ts
+++ src/app/components/world/world.component.ts
@@ -6,27 +6,45 @@ import {
   ElementRef
 } from "@angular/core";
 import { isPlatformBrowser } from "@angular/common";
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: "app-world",
   templateUrl: "./world.component.html",
-  styleUrls: ["./world.component.css"]
+  styleUrls: ["./world.component.css"],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateX(360deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateX(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])  
+  ]
 })
 export class WorldComponent implements OnInit {
   exploreWorld: string = "simpleRotate";
   generatePosition: number;
+  flip: string = 'inactive';
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {}
 
+  randomPosition() {
+    this.generatePosition = Math.floor(Math.random() * 1600);
+    return this.generatePosition + 'px';
+  }
+
   findPlace() {
     console.log("find you destination");
     setTimeout(() => {
       this.exploreWorld = "reachYourDestination";
+      this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
     }, 1000);
     this.exploreWorld = "findingDestination";
-
-    this.generatePosition = Math.floor(Math.random() * 1600);
   }
 }
