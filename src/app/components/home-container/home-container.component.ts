aa58b7dcd3c3a74364ca761292f568bf2e27354e
CodeDriller <me@codedriller.com>
2020-05-13 18:58:55 +0530

    canonical revemp 3.0

--- src/app/components/home-container/home-container.component.ts
+++ src/app/components/home-container/home-container.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { HomeFormService } from '../home-form/home-form.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Inject } from "@angular/core";
+import { HomeFormService } from "../home-form/home-form.service";
+import { ActivatedRoute } from "@angular/router";
+import { DOCUMENT } from '@angular/common';
 
 @Component({
-  selector: 'app-home-container',
-  templateUrl: './home-container.component.html',
-  styleUrls: ['./home-container.component.css']
+  selector: "app-home-container",
+  templateUrl: "./home-container.component.html",
+  styleUrls: ["./home-container.component.css"],
 })
 export class HomeContainerComponent implements OnInit {
-  constructor(private homeFormService : HomeFormService, private route : ActivatedRoute) { }
+  constructor(
+    private homeFormService: HomeFormService,
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) private doc
+  ) {}
 
   ngOnInit() {
     // console.log(this.route.url);
-  }
 
+    let link: HTMLLinkElement = this.doc.createElement("link");
+    link.setAttribute("rel", "canonical");
+    this.doc.head.appendChild(link);
+    link.setAttribute(
+      "href",
+      "https://visa2fly.com"
+    );
+  }
 }
