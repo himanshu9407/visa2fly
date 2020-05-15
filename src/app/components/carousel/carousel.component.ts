e9c7a07117278dba116844232ebea53e1d1103c7
CodeDriller <me@codedriller.com>
2020-05-15 12:45:12 +0530

    revemp home title

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -9,22 +9,11 @@ import { Title, Meta} from '@angular/platform-browser';
 })
 export class CarouselComponent implements OnInit {
 
-  title: string = 'Apply Visa Online | Online Visa Application | Visa2fly';
+ 
 
-  constructor(private titleService: Title,private meta: Meta) { }
+  constructor() { }
 
   ngOnInit() {
-
-    this.titleService.setTitle(this.title);
-    this.meta.addTags([
-      { name: "keywords", content: "Visa Online - Apply for tourist visa online to top international destinations like Dubai, UK, US, Singapore, Schengen, etc. hassle-free with Visa2fly. Click here to know more!" },
-      {
-        name: "description",
-        content: "Apply Visa Online by filling your Online Visa Application at Visa2Fly. Experience hassle-free end to end visa assistance for Indian passport holders. Book Here"
-      },
-      // { name: "author", content: "rsgitech" },
-      // { name: "robots", content: "index, follow" }
-    ]);
   }
 
 
--- src/app/components/home-container/home-container.component.ts
+++ src/app/components/home-container/home-container.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from "@angular/core";
 import { HomeFormService } from "../home-form/home-form.service";
 import { ActivatedRoute } from "@angular/router";
 import { DOCUMENT } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: "app-home-container",
@@ -12,11 +13,24 @@ export class HomeContainerComponent implements OnInit {
   constructor(
     private homeFormService: HomeFormService,
     private route: ActivatedRoute,
-    @Inject(DOCUMENT) private doc
+    @Inject(DOCUMENT) private doc,
+    private titleService: Title,
+    private meta: Meta
   ) {}
 
   ngOnInit() {
     // console.log(this.route.url);
+
+    this.titleService.setTitle("Visa Online | Apply Visa Online | Online Visa Application | Visa2fly");
+    this.meta.addTags([
+      { name: "keywords", content: "Visa Online - Apply for tourist visa online to top international destinations like Dubai, UK, US, Singapore, Schengen, etc. hassle-free with Visa2fly. Click here to know more!" },
+      {
+        name: "description",
+        content: "Apply Visa Online by filling your Online Visa Application at Visa2Fly. Experience hassle-free end to end visa assistance for Indian passport holders. Book Here"
+      },
+      // { name: "author", content: "rsgitech" },
+      // { name: "robots", content: "index, follow" }
+    ]);
 
     let link: HTMLLinkElement = this.doc.createElement("link");
     link.setAttribute("rel", "canonical");
