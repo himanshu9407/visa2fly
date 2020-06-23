74a21cca3ff680f7fc68eb95c58b2258d47328fd
CodeDriller <me@codedriller.com>
2020-06-23 14:43:45 +0530

    serve issue

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ServerCookiesModule } from '@ngx-utils/cookies/server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    ServerCookiesModule.forRoot(),
     AppModule,
     ServerModule,
   ],
