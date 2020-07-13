f7151e5aeaa8a8da301dadcf224618c79d034b0d
CodeDriller <me@codedriller.com>
2020-07-13 19:04:17 +0530

    landscape issue

--- src/app/shared/getIP.service.ts
+++ src/app/shared/getIP.service.ts
@@ -1,19 +1,13 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Injectable({
-    providedIn: "root"
+  providedIn: "root",
 })
 export class GetIPService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http : HttpClient) {}
-
-
-    
-
-     getClientIP  ()    {
-        return   this.http.get('https://api.ipify.org?format=json');
-        
-    }
-    
+  getClientIP() {
+    return this.http.get("https://api.ipify.org?format=json");
+  }
 }
