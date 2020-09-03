a527840153fc64a880d2fe9edcb0d134f13d06dc
Himanshu chopra <anjan9407@gmail.com>
2020-09-03 13:28:43 +0530

    Pull from revempui and malaysia country updated

--- src/app/components/visa-requirements/malaysia/malaysia.component.ts
+++ src/app/components/visa-requirements/malaysia/malaysia.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { ActivatedRoute, Router } from '@angular/router';
 import { VisaRequirementService } from '../visa-requirement.service';
 import { UserFlowDetails } from 'src/app/shared/user-flow-details.service';
-import { LoginStatusService } from 'src/app/shared/login-status.service';
-import { LoginService } from '../../login-signup/login/login.service';
+
 import { PreloaderService } from 'src/app/shared/preloader.service';
 import { ToastrService } from 'ngx-toastr';
-import { RouterHistory } from 'src/app/shared/router-history.service';
-import { RequirementsService } from '../../requirements/requirements.service';
+
 import { Title, Meta } from '@angular/platform-browser';
 import { DOCUMENT } from '@angular/common';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-malaysia',
@@ -36,51 +35,51 @@ import { DOCUMENT } from '@angular/common';
 })
 export class MalaysiaComponent implements OnInit {
 
-  @ViewChild("t") t;
   ngbTabTitleClass;
 
   selectedRequirement: boolean = false;
-
-  // public selectedCountryType = "Malaysia";
+  // selectedRequirement: boolean = false;
+  selectedPurpose: Subject<any> = new Subject();
+  
   public selectedVisaType = "Tourist";
-  desktopJustify = "justified";
-  desktopOrientation = "horizontal";
-  userControlDetail : any;
-  public MyQuotation : Array<any> = [];
-  public MyQuotation1 : Array<any> = [];
-  public imagefield1 : Array<any> = [];
-  public purposeChooseForm : FormGroup;
+  userControlDetail: any;
+  public MyQuotation: Array<any> = [];
+  public MyQuotation1: Array<any> = [];
+  public imagefield1: Array<any> = [];
+  public purposeChooseForm: FormGroup;
+  public onlinestatus: boolean = false;
   // public selectedPurpose = 'Tourist';
-  businessArr : Array<any> =[];
-  touristArr : Array<any> =[];
-  transitArr : Array<any> =[];
+  businessArr: Array<any> = [];
+  touristArr: Array<any> = [];
+  transitArr: Array<any> = [];
   selectedBusiness: number = 1;
   selectedTransit: number = 1;
   selectedTourist: number = 1;
-  public selectedCountrytype = 'Malaysia';
-  public onlinestatus: boolean = false;
+  selectedMobileTourist: number = 1;
+  selectedMobileBusiness: number = 1;
+  selectedMobileTransit: number = 1;
+
+  public selectedCountrytype="Malaysia";
+
   public imageCatogory: Array<any> = [];
   public imageCatogoryBusinessTemp: Array<any> = [];
   public imageCatogoryTouristTemp: Array<any> = [];
   public imageCatogoryTransitTemp: Array<any> = [];
   public imageCatogoryTemp: Array<any> = [];
-  public imageUpload1: Array<any> = [];
+  activeTouristArr: Array<any> = [];
+
 
   constructor(
-    private activeRoute: ActivatedRoute,
     private router: Router,
     private requireQuotation: VisaRequirementService,
     private userFlow: UserFlowDetails,
-    private loginStatus: LoginStatusService,
-    private loginService: LoginService,
-    private preloaderService: PreloaderService,
-    private routerHistory: RouterHistory,
-    private reqService: RequirementsService,
-    private titleService: Title,
     private toastr: ToastrService,
+    private preloaderService: PreloaderService,
+    private titleService: Title,
     private meta: Meta,
     private activatedRoute: ActivatedRoute,
-    @Inject(DOCUMENT) private doc
+    @Inject(DOCUMENT) private doc,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {
     this.activatedRoute.params.subscribe((params) => {
       if (params["purpose"]) {
@@ -97,34 +96,26 @@ export class MalaysiaComponent implements OnInit {
     }
 
     let tempPurpose = this.selectedVisaType;
-    //console.log(tempPurpose);
+    this.userFlow.setUserFlowDetails("country", this.selectedCountrytype);
+
     this.purposeChooseForm = new FormGroup({
-      purposeSelected: new FormControl(tempPurpose)
+      purposeSelected: new FormControl(tempPurpose),
     });
     this.requireQuotation
       .getRequireQuotation(this.selectedCountrytype)
       .subscribe((res: any) => {
-        // console.log(res);
         if (res.code == 0) {
           this.MyQuotation = res.data.quotations;
-
           this.imageCatogory.push(res.data.imageUploadInfo);
-
           this.imageCatogoryBusinessTemp = this.imageCatogory[0]["BUSINESS"];
-          // console.log(this.imageCatogoryBusinessTemp);
-
           this.imageCatogoryTouristTemp = this.imageCatogory[0]["TOURIST"];
-          // console.log(this.imageCatogoryTouristTemp);
-
           this.imageCatogoryTransitTemp = this.imageCatogory[0]["TRANSIT"];
-          // console.log(this.imageCatogoryTransitTemp);
           this.onlinestatus = res.data.onlineCategory;
-
           this.userFlow.setUserFlowDetails(
             "onlineCountry",
             JSON.stringify(res.data.onlineCategory)
           );
-        
+
           this.MyQuotation.forEach((element) => {
             if (element.purpose == "Business") {
               this.businessArr.push(element);
@@ -151,6 +142,11 @@ export class MalaysiaComponent implements OnInit {
             this.router.navigate(["visa/"]);
           }
 
+          this.userFlow.setUserFlowDetails(
+            "imageUploads",
+            JSON.stringify(this.imageCatogoryTemp)
+          );
+
           setTimeout(() => {
             this.preloaderService.showPreloader(false);
           }, 500);
@@ -159,9 +155,7 @@ export class MalaysiaComponent implements OnInit {
             this.preloaderService.showPreloader(false);
             this.router.navigate(["/"]);
           }, 2000);
-          this.toastr.error(
-            "Country Not Found"
-          );
+          this.toastr.error("Country Not Found");
         }
       });
   }
@@ -245,152 +239,61 @@ export class MalaysiaComponent implements OnInit {
     link.setAttribute("href", "https://visa2fly.com/visa/malaysia-visa-online");
   }
 
-  ngAfterViewInit() {
-    this.t.select(this.selectedVisaType);
-  }
-
   purposeChanged() {
     var purpose = this.purposeChooseForm.get("purposeSelected").value;
     this.userFlow.setCookie("selectedVisaPurpose", purpose);
+
     if (purpose == "Tourist") {
       this.MyQuotation1 = this.touristArr;
       this.imageCatogoryTemp = this.imageCatogoryTouristTemp;
-      this.t.select("Tourist");
+      this.selectedPurpose.next(purpose);
     } else if (purpose == "Business") {
       this.MyQuotation1 = this.businessArr;
       this.imageCatogoryTemp = this.imageCatogoryBusinessTemp;
-      this.t.select("Business");
+      this.selectedPurpose.next(purpose);
     } else {
       this.MyQuotation1 = this.transitArr;
       this.imageCatogoryTemp = this.imageCatogoryTransitTemp;
-      this.t.select("Transit");
+      this.selectedPurpose.next(purpose);
     }
+
+    this.userFlow.setUserFlowDetails(
+      "imageUploads",
+      JSON.stringify(this.imageCatogoryTemp)
+    );
   }
 
   navigateTo(purpose: any) {
-    // window.location
-    //let urlpurpose = this.MyQuotation1
-
     let purposeString: string = purpose.nextId;
-    // console.log(purposeString);
     let purposeUrl =
       purposeString.charAt(0).toUpperCase() + purposeString.slice(1);
     this.purposeChooseForm.get("purposeSelected").setValue(purposeString);
     if (purposeString == "Tourist") {
       this.MyQuotation1 = this.touristArr;
+      this.imageCatogoryTemp = this.imageCatogoryTouristTemp;
       this.selectedVisaType = "Tourist";
       this.selectedTourist = 1;
-      this.imageCatogoryTemp = this.imageCatogoryTouristTemp;
-
-      //this.t.select("Tourist");
+      this.selectedMobileTourist = 1;
     } else if (purposeString == "Business") {
       this.MyQuotation1 = this.businessArr;
+      this.imageCatogoryTemp = this.imageCatogoryBusinessTemp;
       this.selectedVisaType = "Business";
       this.selectedBusiness = 1;
-      this.imageCatogoryTemp = this.imageCatogoryBusinessTemp;
-
-      // console.log(this.MyQuotation1);
-      //this.t.select("Business");
+      this.selectedMobileBusiness = 1;
     } else {
       this.MyQuotation1 = this.transitArr;
+      this.imageCatogoryTemp = this.imageCatogoryTransitTemp;
       this.selectedVisaType = "Transit";
       this.selectedTransit = 1;
-      this.imageCatogoryTemp = this.imageCatogoryTransitTemp;
-
-      //this.t.select("Transit");
+      this.selectedMobileTransit = 1;
     }
 
-    this.imagefield1 = this.imageCatogoryTemp;
-
     this.userFlow.setCookie("selectedVisaPurpose", purposeUrl);
-  }
 
-  setActiveTourist(index: number) {
-    this.selectedTourist = index;
-    // console.log('business');
-  }
-
-  setActiveBusiness(index: number) {
-    this.selectedBusiness = index;
-    //  console.log('business');
-  }
-
-  setActiveTransit(index: number) {
-    this.selectedTransit = index;
-    // console.log('business');
-  }
-
-  resetPage() {
-    this.userFlow.setCookie("selectedVisaPurpose", "Tourist");
-  }
-
-  navigate(
-    quoteId: string,
-    purpose: string,
-    category: string,
-    minTravelDate: number,
-    basePrice: number,
-    serviceTax: number,
-    stayPeriod: string,
-    imageUpload: boolean,
-  ) {
-    this.preloaderService.showPreloader(true);
-
-    this.userFlow.setUserFlowDetails("country", this.selectedCountrytype);
-    this.userFlow.setUserFlowDetails("purpose", purpose);
-    this.userFlow.setUserFlowDetails("quoteId", quoteId);
-    this.userFlow.setUserFlowDetails("category", category);
-    this.userFlow.setUserFlowDetails(
-      "minTravelDate",
-      JSON.stringify(minTravelDate)
-    );
-    this.userFlow.setUserFlowDetails("basePrice", JSON.stringify(basePrice));
-    this.userFlow.setUserFlowDetails("serviceTax", JSON.stringify(serviceTax));
-    this.userFlow.setUserFlowDetails("stayPeriod", stayPeriod);
-    this.userFlow.setUserFlowDetails("imageUpload", JSON.stringify(imageUpload));
     this.userFlow.setUserFlowDetails(
       "imageUploads",
       JSON.stringify(this.imageCatogoryTemp)
     );
-
-    let token = this.loginService.getAuthToken();
-    if (token == null || token == undefined) {
-      token = "";
-    }
-    this.loginStatus.verifyAuthToken(token).subscribe((data: any) => {
-      if (data.code == "0") {
-        this.reqService.verifyQuotation(quoteId).subscribe((data: any) => {
-          // console.log(data);
-
-          if (data.code == "0") {
-            this.routerHistory.pushHistory("visa-requirement");
-            this.router.navigate(["addTraveller"]);
-
-            // setTimeout(() => {
-
-            this.preloaderService.showPreloader(false);
-            // }, 2000);
-          } else {
-            this.toastr.error("" + data.message);
-            this.preloaderService.showPreloader(false);
-          }
-        });
-      } else if (data.code == "301") {
-        this.loginService.setAuthToken("");
-        this.loginStatus.setUserStatus(false);
-        this.loginStatus.setUserLoggedIn(false);
-        // this.router.navigate(['visa']);
-        this.preloaderService.showPreloader(false);
-        this.userFlow.setCookie("profile", JSON.stringify({}));
-        this.routerHistory.pushHistory("req-and-quote");
-        this.router.navigate(["slcontainer/login"]);
-        this.preloaderService.showPreloader(false);
-      } else {
-        this.routerHistory.pushHistory("req-and-quote");
-        this.router.navigate(["slcontainer/login"]);
-        this.preloaderService.showPreloader(false);
-      }
-    });
   }
 
 }
--- src/app/components/visa-requirements/malaysia/requirement-malaysia/requirement-malaysia.component.ts
+++ src/app/components/visa-requirements/malaysia/requirement-malaysia/requirement-malaysia.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-requirement-malaysia',
   templateUrl: './requirement-malaysia.component.html',
-  styleUrls: ['./requirement-malaysia.component.css']
+  styleUrls: ['./requirement-malaysia.component.css'],
+  animations: [
+    trigger("simpleFadeAnimation", [
+      state("in", style({ opacity: 1 })),
+      transition(":enter", [style({ opacity: 0 }), animate(800)]),
+      transition(
+        ":leave",
+        animate(800, style({ opacity: 0, background: "green" }))
+      ),
+    ]),
+  ],
 })
 export class RequirementMalaysiaComponent implements OnInit {
 
+  desktopJustify = "justified";
+  desktopOrientation = "horizontal";
+  mobileOrientation = "vertical";
+
+  @ViewChild("t") t;
+
+  // @Input() t;
+  @Input() selectedBusiness: number;
+  @Input() selectedTransit: number;
+  @Input() selectedTourist: number;
+  @Input() selectedMobileTourist: number;
+  @Input() selectedMobileBusiness: number;
+  @Input() selectedMobileTransit: number;
+  @Input() selectedVisaType: string;
+  @Input() selectedPurpose: Subject<any>;
+
+  @Output() changedPurpose = new EventEmitter();
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.selectedPurpose.subscribe((res) => {
+      this.t.select(res);
+    })
+  }
+
+  ngAfterViewInit() {
+    this.t.select(this.selectedVisaType);
+  }
+
+  setActiveTourist(index: number, id: string) {
+    this.selectedTourist = index;
+
+    if ($("#tourist" + index).hasClass("show")) {
+      $("#" + id).removeClass("showDiv");
+    } else {
+      $("#" + id).addClass("showDiv");
+    }
+  }
+
+  setActiveTouristMobile(index: number, id: string) {
+    this.selectedMobileTourist = index;
+
+    if ($("#touristMobile" + index).hasClass("show")) {
+      $("#" + id).removeClass("showDiv");
+    } else {
+      $("#" + id).addClass("showDiv");
+    }
+  }
+
+  setActiveBusiness(index: number, id: string) {
+    this.selectedBusiness = index;
+
+    if ($("#business" + index).hasClass("show")) {
+      $("#" + id).removeClass("showDiv");
+    } else {
+      $("#" + id).addClass("showDiv");
+    }
+  }
+
+  setActiveBusinessMobile(index: number, id: string) {
+    this.selectedMobileBusiness = index;
+
+    if ($("#businessMobile" + index).hasClass("show")) {
+      $("#" + id).removeClass("showDiv");
+    } else {
+      $("#" + id).addClass("showDiv");
+    }
+  }
+
+  setActiveTransit(index: number) {
+    this.selectedTransit = index;
+  }
+
+  changePurpose(event) {
+    this.changedPurpose.emit(event);
   }
 
 }
