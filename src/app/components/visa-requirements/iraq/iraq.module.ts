81f3e3015aa2cfa46e1abbb9f0e1de9a768f04ae
ksbisht <ksbisht941@gmail.com>
2020-12-21 15:27:28 +0530

    iraq done

--- src/app/components/visa-requirements/iraq/iraq.module.ts
+++ src/app/components/visa-requirements/iraq/iraq.module.ts
@@ -3,13 +3,34 @@ import { CommonModule } from '@angular/common';
 import { IraqComponent } from './iraq.component';
 import { ImportantPointsComponent } from './important-points/important-points.component';
 import { QuotationComponent } from './quotation/quotation.component';
+import { FaqComponent } from './faq/faq.component';
+import { RequirementComponent } from './requirement/requirement.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedVisaModuleModule } from '../../shared-visa-module/shared-visa-module.module';
+import { FinlandComponent } from '../finland/finland.component';
 
+const routes: Routes = [
+  { path: "", component: FinlandComponent },
+]
 
 
 @NgModule({
-  declarations: [IraqComponent, ImportantPointsComponent, QuotationComponent],
+  declarations: [
+    IraqComponent,
+    ImportantPointsComponent,
+    QuotationComponent,
+    FaqComponent,
+    RequirementComponent
+  ],
   imports: [
-    CommonModule
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    NgbModule,
+    SharedVisaModuleModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class IraqModule { }
