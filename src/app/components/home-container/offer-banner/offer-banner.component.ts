2a30420e6f206811db0b1fa2b69d333bd1056864
ksbisht <ksbisht941@gmail.com>
2021-03-04 01:20:03 +0530

    fuck up

--- src/app/components/home-container/offer-banner/offer-banner.component.ts
+++ src/app/components/home-container/offer-banner/offer-banner.component.ts
@@ -10,6 +10,7 @@ export class OfferBannerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/components/offers/mobik/mobik.component.ts
+++ src/app/components/offers/mobik/mobik.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mobik',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobikComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title, private meta: Meta) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle("Visa2fly | Offers");
+    this.meta.addTags([
+      { name: "keywords", content: "Visa2fly Offers" },
+      {
+        name: "description",
+        content: "Get Flat ₹ 250 Cashback from MobiKwik",
+      },
+    ]);
   }
 
 }
